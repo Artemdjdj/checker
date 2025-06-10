7d2ce7d0dd4a64587b25c8e3f17fe018b0f2abcc
Artemdjdj <artembasuraa@gmail.com>
2025-06-10 17:12:01 +0300

    solve problem with qeen and color theme in settings

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from PySide6 import *
 import sys
 import os
 from form import *
+from settings import *
 from form_settings import *
 from game import *
 from utils import *
@@ -14,6 +15,7 @@ class MainWindow(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.temp = self.ui.first_cenral_widget
+        self.settings_window = None
         # Явно устанавливаем состояние окна
         self.setWindowState(Qt.WindowNoState)
         # Разрешаем все операции с окном
@@ -42,7 +44,15 @@ class MainWindow(QMainWindow):
                             self.ui.button_50, self.ui.button_51, self.ui.button_52, self.ui.button_53, self.ui.button_54, self.ui.button_55,self.ui.button_56, self.ui.button_57,
                             self.ui.button_60, self.ui.button_61, self.ui.button_62, self.ui.button_63, self.ui.button_64, self.ui.button_65,self.ui.button_66, self.ui.button_67,
                             self.ui.button_70, self.ui.button_71, self.ui.button_72, self.ui.button_73, self.ui.button_74, self.ui.button_75,self.ui.button_76, self.ui.button_77,]
-        
+        self.all_lines = [
+            [[6,0],[7,1]],
+            [[4,0],[5,1],[6,2],[7,3]],
+            [[2,0],[3,1],[4,2],[5,3],[6,4],[7,5]],
+            [[0,0],[1,1],[2,2],[3,3],[4,4],[5,5],[6,6], [7,7]],
+            [[0,2],[1,3],[2,4],[3,5],[4,6],[5,7]],
+            [[0,4],[1,5],[2,6],[3,7]],
+            [[0,6],[1,7]],
+        ]      
         self.add_functions()
         self.start_settings()
         self.ui.left_widget.setCurrentWidget(self.ui.first_tab_left)
@@ -51,7 +61,8 @@ class MainWindow(QMainWindow):
         self.ui.right_widget.tabBar().hide()
         self.ui.central_widget.setCurrentWidget(self.ui.first_cenral_widget)
         self.ui.right_widget.setCurrentWidget(self.ui.first_widget_right)
-
+        self.style_buttons = style_classic
+        self.style_around_buttons = style_around_classic
     def start_settings(self):
         for i in range(0,3):
             if i%2 == 0: 
@@ -89,6 +100,7 @@ class MainWindow(QMainWindow):
         self.ui.back_to_report.clicked.connect(lambda:self.back_rep())
         self.ui.leave_the_game.clicked.connect(lambda:self.get_away())
         self.ui.stop_game.clicked.connect(lambda:self.go_to_preview())
+        self.ui.settings.clicked.connect(lambda:self.open_settings())
         for button in self.all_buttons:
             button.clicked.connect(lambda ch, b=button: self.choose_start_position(b))
 
@@ -102,84 +114,107 @@ class MainWindow(QMainWindow):
         index_of_col = int(button.objectName()[-1])
         figure_type = self.board[index_of_row][index_of_col].type_of_figure
         
+        
         if figure_type == self.type_of_figure: 
-            button.setStyleSheet(style1)
+            button.setStyleSheet(self.style_around_buttons)
             self.name_parent_button = button.objectName()
             if self.parent_r != -1 and self.parent_c != -1:
                 name = f"button_{self.parent_r}{self.parent_c}"
                 parent_button = self.get_button_by_name(name)
-                parent_button.setStyleSheet(style2)
+                parent_button.setStyleSheet(self.style_buttons)
             self.parent_r = index_of_row
             self.parent_c = index_of_col
         
         else:
+            
+            if self.parent_r == -1 or self.parent_c == -1 or self.board[self.parent_r][self.parent_c].type_of_figure != self.type_of_figure:
+                return
+                
             check_is_qeen = self.board[self.parent_r][self.parent_c].isqeen
             ways = []
             more_important_ways = []
             list_buttons_that_must_to_cut_ohter = []
             result = []
-            self.get_buttons_that_should_cut_others(list_buttons_that_must_to_cut_ohter,self.board[self.parent_r][self.parent_c].type_of_figure)
+           
+            self.get_buttons_that_should_cut_others(list_buttons_that_must_to_cut_ohter, self.type_of_figure)  
             
-            self.get_all_ways(self.parent_r, self.parent_c, ways, more_important_ways, check_is_qeen, self.board[self.parent_r][self.parent_c].type_of_figure)
-            print()
-            print("more_importants_way")
-            for t in more_important_ways:
-                print(t)
+            self.get_all_ways(self.parent_r, self.parent_c, ways, more_important_ways, check_is_qeen, self.type_of_figure,-1,-1) 
+            
             if check_is_qeen:
-                if self.pos_which_can_cut_figure(more_important_ways, result, self.board[self.parent_r][self.parent_c].type_of_figure):
+                if self.pos_which_can_cut_figure(more_important_ways, result, self.type_of_figure): 
                     more_important_ways.clear()
-                    print()
-                    print("result")
                     for i in result:
                         more_important_ways.append(i)
-                        print(i)
             
+            
+            if figure_type is not None:
+                return
+                
             if len(list_buttons_that_must_to_cut_ohter) == 0:
                 if len(more_important_ways) == 0 and len(ways) > 0:
-                    self.change_position(ways, figure_type, index_of_row, index_of_col,self.board[self.parent_r][self.parent_c].type_of_figure, is_capture=False,)
+                    self.change_position(ways, figure_type, index_of_row, index_of_col, self.type_of_figure, is_capture=False)
             else:
-                # Если есть обязательные взятия
-                if figure_type == None and [self.parent_r, self.parent_c] in list_buttons_that_must_to_cut_ohter:
+                
+                if [self.parent_r, self.parent_c] in list_buttons_that_must_to_cut_ohter:
                     if [index_of_row, index_of_col] in more_important_ways:
                         name = f"button_{index_of_row}{index_of_col}"
                         new_button = self.get_button_by_name(name)
                         parent_button = self.get_button_by_name(self.name_parent_button)
                         icon = parent_button.icon()
-                        #вычисление напрваления сбиваемой шашки
-                        d_row = 1 if index_of_row> self.parent_r else -1
-                        d_col = 1 if index_of_col> self.parent_c else -1
+                        
+                        d_row = 1 if index_of_row > self.parent_r else -1
+                        d_col = 1 if index_of_col > self.parent_c else -1
                         cut_row, cut_col = self.parent_r, self.parent_c
-                        time_r, time_c = self.parent_r+d_row, self.parent_c+d_col
-                        while (time_r, time_c)!= (index_of_row, index_of_col) and  0 <= time_r < 8 and 0 <= time_c < 8:
+                        time_r, time_c = self.parent_r + d_row, self.parent_c + d_col
+                        
+                        
+                        while (time_r, time_c) != (index_of_row, index_of_col) and 0 <= time_r < 8 and 0 <= time_c < 8:
                             if self.board[time_r][time_c].type_of_figure is not None:
-                                cut_row, cut_col = time_r, time_c
-
-                            time_r+=d_row
-                            time_c+=d_col
-                        # cut_row = (index_of_row + self.parent_r) // 2
-                        # cut_col = (index_of_col + self.parent_c) // 2
+                                if self.board[time_r][time_c].type_of_figure != self.type_of_figure:
+                                    cut_row, cut_col = time_r, time_c
+                                else:
+                                    return  
+                            time_r += d_row
+                            time_c += d_col
+                            
                         name_of_cut = f"button_{cut_row}{cut_col}"
                         cut_button = self.get_button_by_name(name_of_cut)
-                        
-                        # Очищаем срубленную фигуру
+                        if self.board[cut_row][cut_col].type_of_figure == self.type_of_figure:
+                            return
+                            
                         self.board[cut_row][cut_col].type_of_figure = None
                         self.board[cut_row][cut_col].isbusy = False
                         self.cut_figures.append(cut_button.objectName())
                         
-                        # Обновляем счетчик фигур
                         types = "black" if self.type_of_figure == "white" else "white"
                         if types == "black":
                             self.count_of_black_figure -= 1
                         else:
                             self.count_of_white_figure -= 1
                         
-                        self.change_position(more_important_ways, figure_type, index_of_row, index_of_col,self.board[self.parent_r][self.parent_c].type_of_figure, is_capture=True)
+                        self.change_position(more_important_ways, figure_type, index_of_row, index_of_col, self.type_of_figure, is_capture=True)
 
     def remove_cut_figures(self):
         for n in self.cut_figures:
             button = self.get_button_by_name(n)
             button.setIcon(QIcon())
         self.cut_figures.clear()
+
+    def find_cut_figure(self, index_of_row, index_of_col):
+        d_row = 1 if index_of_row > self.parent_r else -1
+        d_col = 1 if index_of_col > self.parent_c else -1
+        cut_row, cut_col = self.parent_r, self.parent_c
+        time_r, time_c = self.parent_r + d_row, self.parent_c + d_col               
+        while (time_r, time_c) != (index_of_row, index_of_col) and 0 <= time_r < 8 and 0 <= time_c < 8:
+            if self.board[time_r][time_c].type_of_figure is not None:
+                if self.board[time_r][time_c].type_of_figure != self.type_of_figure:
+                    cut_row, cut_col = time_r, time_c
+                else:
+                    return  
+            time_r += d_row
+            time_c += d_col
+        return [cut_row, cut_col]
+
 
     def change_position(self, ways, figure_type, index_of_row, index_of_col, types,is_capture):
         if figure_type == None and [index_of_row, index_of_col] in ways:
@@ -189,7 +224,7 @@ class MainWindow(QMainWindow):
             icon = parent_button.icon()
             new_button.setIcon(icon)
             parent_button.setIcon(QIcon())
-            parent_button.setStyleSheet(style2)
+            parent_button.setStyleSheet(self.style_buttons)
             
             is_queen = self.board[self.parent_r][self.parent_c].isqeen
             self.board[index_of_row][index_of_col].type_of_figure = self.board[self.parent_r][self.parent_c].type_of_figure
@@ -204,12 +239,12 @@ class MainWindow(QMainWindow):
                 ways = []
                 more_important_ways = []
                 current_type = self.board[index_of_row][index_of_col].type_of_figure
-                self.get_all_ways(index_of_row, index_of_col, ways, more_important_ways, self.board[index_of_row][index_of_col].isqeen,current_type)
+                self.get_all_ways(index_of_row, index_of_col, ways, more_important_ways, self.board[index_of_row][index_of_col].isqeen,current_type,-1,-1)
                 if len(more_important_ways) > 0:
                     self.name_parent_button = name
                     self.parent_r = index_of_row
                     self.parent_c = index_of_col
-                    new_button.setStyleSheet(style1)
+                    new_button.setStyleSheet(self.style_around_buttons)
                     return
             
                 self.remove_cut_figures()
@@ -229,7 +264,7 @@ class MainWindow(QMainWindow):
                 if len(more_important_ways)>0:
                     lists.append([row, col])
 
-    def get_all_ways(self, row, col, ways, more_important_ways, is_qeen, types):
+    def get_all_ways(self, row, col, ways, more_important_ways, is_qeen, types, nerow=-1, necol=-1):
         figure = self.board[row][col]
         if not is_qeen:
             if types=="white":
@@ -260,24 +295,30 @@ class MainWindow(QMainWindow):
                     more_important_ways.append([row+2, col+2])
 
         else:
-            count=1
-            while (row-count>-1 and col-count>-1) or (row-count>-1 and col+count<8) or (row+count<8 and col-count>-1) or (row+count<8 and col+count<8):
-                if row-count>-1 and col-count>-1 and self.board[row-count][col-count].type_of_figure is None:
-                    ways.append([row-count, col-count])
-                if row-count>-1 and col+count<8 and self.board[row-count][col+count].type_of_figure is None:
-                    ways.append([row-count, col+count])
-                if row+count<8 and col-count>-1 and self.board[row+count][col-count].type_of_figure is None:
-                    ways.append([row+count, col-count])
-                if row+count<8 and col+count<8 and self.board[row+count][col+count].type_of_figure is None:
-                    ways.append([row+count, col+count])
-                count+=1
-                            
+            types_fig = None
+            if nerow!=-1 or necol!=-1:
+                print(f"{nerow}{necol}")
+                types_fig = self.board[nerow][necol].type_of_figure
+                self.board[nerow][necol].type_of_figure = None
+            count=1    
             directions = [
                 (-1, -1),  # левый низ
                 (-1, 1),   # левый верх
                 (1, -1),   # правый низ
                 (1, 1)     # правый верх
             ]
+            for dr, dc in directions:
+                count = 1
+                while True:
+                    r = row + count * dr
+                    c = col + count * dc
+                    if not (0 <= r < 8 and 0 <= c < 8):
+                        break
+                    if self.board[r][c].type_of_figure is None:
+                        ways.append([r,c])
+                        count += 1
+                    else:
+                        break
                             
             for dr, dc in directions:
                 count = 1
@@ -287,7 +328,6 @@ class MainWindow(QMainWindow):
                     r = row + count * dr
                     c = col + count * dc
                                     
-                                # Проверка выхода за пределы доски
                     if not (0 <= r < 8 and 0 <= c < 8):
                         break
                                     
@@ -313,20 +353,35 @@ class MainWindow(QMainWindow):
                             count += 1
                         else:
                             # Любая фигура за врагом - остановка
-                            break                
+                            break
+            if types_fig is not None:
+                self.board[nerow][necol].type_of_figure = types_fig
+                     
                     
            
-    def pos_which_can_cut_figure(self, more_important_ways, result,types):
+    def pos_which_can_cut_figure(self, more_important_ways, result, types):
+        
+        # for r, c in more_important_ways:
+        #     new_more_important_ways = []
+        #     new_ways = []
+        #     self.get_all_ways(r, c, new_ways, new_more_important_ways, True, types)
+        #     if len(new_more_important_ways) > max_captures:
+        #         max_captures = len(new_more_important_ways)
+        print("\n more_important_ways:")
+        print(more_important_ways)
         for r, c in more_important_ways:
             new_more_important_ways = []
             new_ways = []
-            self.get_all_ways(r, c, new_ways, new_more_important_ways, True,types)
-            if len(new_more_important_ways)> 0 and r!=self.parent_r and c!=self.parent_c:
-                print()
-                print("куда дальше становиться")
-                for s in new_more_important_ways:
-                    print(s)
+            arr = self.find_cut_figure(r, c)
+            nerow, necol = arr[0], arr[1]
+            self.get_all_ways(r, c, new_ways, new_more_important_ways, True, types, nerow, necol)
+            if len(new_more_important_ways) >0:
+                print(f"\nДля позиции {r}{c} след ходы")
+                print(new_more_important_ways)
                 result.append([r, c])
+        print("\n result:")
+        print(result)
+       
         return len(result) > 0
 
 
@@ -379,8 +434,45 @@ class MainWindow(QMainWindow):
         self.name_parent_button =""
         self.parent_r = -1
         self.parent_c = -1
+    
+    
+    def set_theme_for_board(self, theme):
+        for button in self.all_buttons:
+            row = int(button.objectName()[-2])
+            col = int(button.objectName()[-1])
+            if (row%2 == 0 and col%2==0) or (row%2==1 and col%2==1):
+                if theme=="Темно-зеленая":
+                    button.setStyleSheet(style_for_dark_green_board)
+                    self.style_buttons = style_for_dark_green_board
+                    self.style_around_buttons = style_around_green
+                elif theme == "Светлая":
+                    button.setStyleSheet(style_for_light_yellow_board)
+                    self.style_buttons = style_for_light_yellow_board
+                    self.style_around_buttons = style_around_yellow
+                    
 
+    def open_settings(self):
+        if self.settings_window is None:
+            self.settings_window = SettingsWindow()
+            
+            self.settings_window.ui.pushButton.clicked.connect(lambda:self.on_settings_closed())
+            
+            self.settings_window.ui.pushButton_2.clicked.connect(
+                lambda: self.set_theme_for_board(self.settings_window.ui.comboBox_2.currentText())
+            )
+        self.settings_window.show()
+
+    def on_settings_closed(self):
+        self.settings_window = None
         
+class SettingsWindow(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_Dialog()
+        self.ui.setupUi(self)
+    
+        
+      
 
 if __name__ =="__main__":
     app = QApplication(sys.argv)
@@ -392,6 +484,10 @@ if __name__ =="__main__":
     window.show()
     sys.exit(app.exec())
 
+
+
+
+                   
 
 
 
--- utils.py
+++ utils.py
@@ -1,12 +1,33 @@
 
-style1 = """
+style_around_classic = """
             background-color:rgb(86, 100, 117);
             border:4px solid rgb(113, 255, 62);
             """
 
-style2 = """
+style_classic = """
             background-color:rgb(86, 100, 117);
             border:none;
             """
 qeen_icon_white = u":/icons/icons/white_figure.png"
 qeen_icon_black = u":/icons/icons/black_figure.png"
+
+
+
+style_for_dark_green_board = """
+            background-color:rgb(22, 56, 16);
+            border:none;
+            """
+
+
+style_for_light_yellow_board = """
+            background-color:rgb(255, 161, 84);
+            border:none;
+            """
+style_around_yellow ="""
+            background-color:rgb(255, 161, 84);
+            border:4px solid rgb(255, 56, 38);
+        """
+style_around_green ="""
+            background-color:rgb(22, 56, 16);
+            border:4px solid rgb(29, 158, 158);
+        """
